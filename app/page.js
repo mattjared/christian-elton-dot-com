f906e2a803791de2f7a55b79750af1c167077453
Christian Elton <christian@Christians-MacBook-Pro.local>
2023-06-09 16:00:31 -0600

    good day sir

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          The world's best Homo Sapiens&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
