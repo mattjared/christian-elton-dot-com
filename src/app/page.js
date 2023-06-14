a1b8725bb015d53afa2945344816273803e2a100
Christian Elton <christian@Christians-MacBook-Pro.local>
2023-06-14 09:59:58 -0400

    dwight shrute

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,12 @@ import Footer from "./Footer"
 
 export default function Home() {
   return (
-    <main className='bg-gray-100'>
+    <main className='bg-purple-200'>
       <section className="py-10 md:pt-24 md:pb-12 container max-w-screen-xl mx-auto px-4 text-center">
-        <Image src="/headshot.jpeg" alt="" width="250" height="250" className="mx-auto mb-16 rounded-full" />
-        <h1 className="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">{`my name`}</h1>
-        <h2 className='text-6xl font-bold'>Account Executive @ Vercel</h2>
-        <p className="font-normal text-gray-600 text-md md:text-xl my-16">Selling software and keepign it A1 since Day 1</p>
+        <Image src="/me.png" alt="" width="500" height="500" className="mx-auto mb-16" />
+        <h1 className="font-medium text-purple-900 text-lg md:text-2xl uppercase mb-8">{`Christian Elton`}</h1>
+        <h2 className='text-6xl font-normal'>Manager of the best team @ Vercel</h2>
+        <p className="font-bold text-purple-400 text-md md:text-xl my-16">I could not be an astronaut so I am in sales</p>
       </section>
       <Contact />
       <Tiles />
